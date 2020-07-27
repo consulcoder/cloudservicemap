66ccd44472408bc5757ece36d384a10f2bea52e0
joseangelsalgado <friendlycubavoyage@gmail.com>
2020-07-27 08:30:37 +0200

    last update, no images yet

--- blog/views.py
+++ blog/views.py
@@ -13,14 +13,6 @@ this_path = os.getcwd() + '/blog/'
 from django_xhtml2pdf.utils import generate_pdf
 
 
-def myview(request):
-    resp = HttpResponse(content_type='application/pdf')
-    dynamic_variable = request.user.some_special_something
-    context = {'some_context_variable': dynamic_variable}
-    result = generate_pdf('my_template.html', file_object=resp, context=context)
-    return result
-
-
 def index(request):
     context = {}
     context['servicecompute'] = Service.objects.filter(sous_categorie__categorie_id=2).filter(statut=True)
@@ -33,10 +25,6 @@ def index(request):
     for tree in Tree.objects.filter(in_home=True).order_by('order'):
         context['trees'].append(tree.toArray())
     return render(request, "blog/index.html", context)
-
-
-"""def pdf(request):
-    return render(request, "blog/pdf.html")"""
 
 
 def pdf(request):
@@ -58,12 +46,3 @@ def pdf(request):
     response.write(pdf)
 
     return response
-
-
-"""def testinpdf(request):
-    pdf_buffer = BytesIO()
-    my_doc = SimpleDocTemplate(pdf_buffer)
-    box = {
-        'nom': 'for nom_four'
-               ''
-    }"""
