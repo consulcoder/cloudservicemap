14405eef167446f98c4ab87eaa1fe722928b5f1b
Sam <samrey869@gmail.com>
2021-03-08 15:05:27 -0500

    UP 8/3/2021

--- tool/forms.py
+++ tool/forms.py
@@ -1,1 +1,14 @@
 from django import forms
+
+
+class FormUpload(forms.Form):
+
+    UP = forms.FileField()
+
+    def __init__(self, *args, **kwargs):
+        super(FormUpload, self).__init__(*args, **kwargs)
+
+    def handle_uploaded_file(self, f):
+        with open('media/' + f.name, 'w') as destination:
+            for chunk in f.chunks():
+                destination.write(chunk)
